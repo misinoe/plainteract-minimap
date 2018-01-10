4f538b2a9f84dd60a0be93a488c6cfeaec16a3ec
misinoe <misinoe.t@gmail.com>
2018-01-10 18:40:14 +0900

    add sample

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const {
   Application,
   Graphics,
   Container,
+  Sprite,
   } = Pixi;
 
 const degToRad = Math.PI / 180;
@@ -10,6 +11,24 @@ const radToDeg = 180 / Math.PI;
 import merge from 'merge';
 
 export default class Minimap {
+  static get baseDir() {
+    if (typeof Minimap._baseDir === 'undefined') {
+      Minimap._baseDir = '';
+    }
+    return Minimap._baseDir;
+  }
+  static set baseDir(baseDir) {
+    Minimap._baseDir = baseDir;
+  }
+
+  static normalizePath(path) {
+    let baseDir = Minimap.baseDir;
+    if (path.charAt(0) === '/' || path.slice(0, 4) === 'http') {
+      return path;
+    }
+    return (baseDir ? baseDir + '/' : '') + path;
+  }
+
   constructor(data = {}, options = {}) {
     const application = this.application = new Application();
     const {stage} = application;
@@ -17,31 +36,39 @@ export default class Minimap {
     const field = this.field = new Container();
     stage.addChild(field);
 
-    const nodeStyle = {
-      lineWidth = 3,
-      lineColor = 0xffffff,
-      lineAlpha = 0.8,
-      fillColor = 0xffffff,
-      fillAlpha = 0.5,
-      circleRadius = 5,
-      } = options.nodeStyle || {};
-    this.nodeStyle = nodeStyle;
+    const imageContainer = this.imageContainer = new Container();
+    field.addChild(imageContainer);
+
+    const nodeStyle = this.nodeStyle = merge.recursive(true, {
+      lineWidth: 3,
+      lineColor: 0xffffff,
+      lineAlpha: 0.8,
+      fillColor: 0xffffff,
+      fillAlpha: 0.5,
+      circleRadius: 5,
+      }, options.nodeStyle || {});
     const nodeContainer = this.nodeContainer = new Container();
     field.addChild(nodeContainer);
 
-    const cornStyle = {
-      lineWidth = 2,
-      lineColor = 0xffffff,
-      lineAlpha = 0.4,
-      fillColor = 0xffffff,
-      fillAlpha = 0.5,
-      circleRadius = 5,
-    }  = options.cornStyle || {};
-    this.cornStyle = conrStyle;
-    const corn = this.corn = new Corn(conrStyle);
+    const cornStyle = this.cornStyle = merge.recursive(true, {
+      lineWidth: 2,
+      lineColor: 0xffffff,
+      lineAlpha: 0.4,
+      fillColor: 0xffffff,
+      fillAlpha: 0.5,
+      circleRadius: 5,
+      }, options.cornStyle || {});
+    this.cornStyle = cornStyle;
+    const corn = this.corn = new Corn(cornStyle);
     field.addChild(corn);
 
+    if ('baseDir' in options) {
+      Minimap.baseDir = options.baseDir;
+    }
+
     application.start();
+
+    this.setMinimapData(data);
   }
 
   getView() {
@@ -98,10 +125,46 @@ export default class Minimap {
     const {
       nodeContainer,
       nodeStyle,
+      imageContainer,
       } = this;
+
     nodeContainer.removeChildren();
 
-    const {nodes} = minimapData;
+    const {
+      nodes = [],
+      images = [],
+      } = minimapData;
+
+    imageContainer.removeChildren();
+    images.forEach((imageData) => {
+      const {
+        x = 0,
+        y = 0,
+        width = null,
+        height = null,
+        scaleX = null,
+        scaleY = null,
+        pivotX = null,
+        pivotY = null,
+        rotation = null,
+        path,
+        } = imageData;
+
+      const sprite = Sprite.fromImage(Minimap.normalizePath(path), 'auto');
+
+      if (x) sprite.x = x;
+      if (y) sprite.y = y;
+      if (width) sprite.width = width;
+      if (height) sprite.height = height;
+      if (pivotX) sprite.pivot.x = pivotX;
+      if (pivotY) sprite.pivot.y = pivotY;
+      if (rotation) sprite.rotation = rotation;
+      if (scaleX) sprite.scale.x = scaleX;
+      if (scaleY) sprite.scale.y = scaleY;
+
+      imageContainer.addChild(sprite);
+    });
+
     nodes.forEach((nodeData) => {
       const {
         position = {
